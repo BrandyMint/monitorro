4eb40d1a13988814afc823ed53cfb0d400869ad0
Danil Pismenny <danil@brandymint.ru>
2019-03-23 10:57:39 +0300

    remove bestchange objects

--- app/value_objects/best_change_changer_row.rb
+++
@@ -1,6 +0,0 @@
-class BestChangeChangerRow
-  include Virtus.model strict: true
-
-  attribute :id,   Integer # ID обменнике по таблице BestExchange bm_exch.dat
-  attribute :name, String  # Имя обменника
-end
