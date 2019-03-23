7d59e19fc2db64ab951a13bfbc60e99ef08046ad
Danil Pismenny <danil@brandymint.ru>
2019-03-23 11:00:51 +0300

    sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+require 'sidekiq/web'
+require 'sidekiq-status/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
+  get '/sidekiq-stats' => proc { [200, { 'Content-Type' => 'text/plain' }, [Sidekiq::Stats.new.to_json]] }
+  # Sidekiq::Web.set :session_secret, Secrets.secret_key_base
+  mount Sidekiq::Web => '/sidekiq'
+
   root controller: 'dashboard', action: 'index'
 
   resources :rates, only: [:index]
