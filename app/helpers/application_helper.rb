eb0cf1555a81a7976b807951b1d8b8976ef8e065
Danil Pismenny <danil@brandymint.ru>
2019-03-25 09:08:24 +0300

    show growth arrows

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,18 @@ module ApplicationHelper
     "best rate demo #{AppVersion}"
   end
 
+  def rate_growth(current, last)
+    return content_tag :span, '&middot;'.html_safe, class: 'text-muted' if last.nil?
+    return content_tag :span, '?', class: 'text-warning' if current.nil?
+    if current > last
+      content_tag :span, '&uarr;'.html_safe, class: 'text-success'
+    elsif current < last
+      content_tag :span, '&darr;'.html_safe, class: 'text-danger'
+    else
+      content_tag :span, '&middot;'.html_safe, class: 'text-muted'
+    end
+  end
+
   def humanized_time(time)
     return '-' unless time
 
