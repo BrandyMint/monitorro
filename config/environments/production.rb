3e628b297535ffccb0068f58d84e65e6daccd88b
Danil Pismenny <danil@brandymint.ru>
2019-04-03 16:04:14 +0300

    suppress production logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,10 +51,10 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [ :request_id, :remote_ip, :url ]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -63,11 +63,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "change_finder_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.logger = ActiveSupport::Logger.new Rails.root.join './log/action_mailer.log'
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
